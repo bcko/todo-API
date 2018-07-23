8ca0a9f3fe33e43ae1986e905247b0f7aef647f4
BC Ko <gobcko@gmail.com>
2018-07-22 23:02:45 -0700

    port number is variable

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
+
 const apiRouterV1 = require('./routes/api/v1');
 app.use('/api/v1', apiRouterV1);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('todo api app listening on port ' + port + '!'));
